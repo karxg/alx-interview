7e5cfe1c20887fc220002f24d6c29be9d8c7343c
karxg <karim4420@aol.com>
2024-07-25 17:18:19 +0300

    0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Pascal's Triangle Generator
+"""
+
+def generate_pascals_triangle(rows):
+    """
+    Generates Pascal's Triangle up to a given number of rows.
+
+    Args:
+        rows (int): The number of rows of Pascal's Triangle to generate.
+
+    Returns:
+        list: A list of lists containing the values of Pascal's Triangle.
+              Returns an empty list if rows <= 0.
+    """
+    triangle = []
+    if rows <= 0:
+        return triangle
+
+    triangle = [[1]]
+    for row_index in range(1, rows):
+        row = [1]
+        previous_row = triangle[row_index - 1]
+        for column_index in range(len(previous_row) - 1):
+            row.append(previous_row[column_index] + previous_row[column_index + 1])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
+
