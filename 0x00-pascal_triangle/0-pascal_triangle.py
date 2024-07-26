607b2e11922988ad6c01f1c262deea0e7ec68585
karxg <karim4420@aol.com>
2024-07-26 23:18:48 +0300

    Fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,40 +1,40 @@
 #!/usr/bin/python3
 """
-Create Pascal's Triangle up to a specified number of rows.
+Generate Pascal's Triangle up to the nth row.
 
     Args:
-        rows (int): The number of rows to generate for Pascal's Triangle.
+        n (int): The number of rows of Pascal's Triangle to generate.
 
     Returns:
         list: A list of lists representing Pascal's Triangle.
 """
 
 
-def pascal_triangle(rows):
+def pascal_triangle(n):
     """
-    Create Pascal's Triangle up to a specified number of rows.
+    Generate Pascal's Triangle up to the nth row.
 
     Args:
-        rows (int): The number of rows to generate for Pascal's Triangle.
+        n (int): The number of rows of Pascal's Triangle to generate.
 
     Returns:
         list: A list of lists representing Pascal's Triangle.
     """
-    if rows <= 0:  # If rows is less than or equal to 0, return an empty list.
+    if n <= 0:  # If n is less than or equal to 0, return an empty list.
         return []
-    triangle = []
-    for row_num in range(rows):  # Iterate over the range from 0 to rows-1.
-        if row_num == 0:
-            triangle.append([1])  # The first row is always [1].
-        elif row_num == 1:
-            triangle.append([1, 1])  # The second row is always [1, 1].
+    Triangle = []
+    for i in range(n):  # Iterate over the range from 0 to n-1.
+        if i == 0:
+            Triangle.append([1])
+        elif i == 1:
+            Triangle.append([1, 1])
         else:
-            current_row = [1]  # Start each new row with 1.
-            for j in range(1, len(triangle[row_num - 1])):
-                # Append the sum of the two elements above the current
-                current_row.append(triangle[row_num - 1][j] + triangle[row_num - 1][j - 1])
-            current_row.append(1)  # End each row with 1.
-            triangle.append(current_row.copy())
-            current_row.clear()  # Clear the current_row list
+            row = [1]  # Start each new row with 1.
+            for x in range(1, len(Triangle[i - 1])):
 
-    return triangle  # Return the complete Pascal's Triangle.
+                row.append(Triangle[i - 1][x] + Triangle[i - 1][x - 1])
+            row.append(1)  # End every row with 1.
+            Triangle.append(row.copy())
+            row.clear()  # Clear row list
+
+    return Triangle  # Return the Pascal's Triangle.
