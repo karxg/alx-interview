54d95d35a1eee295aa4344424e054d1d0bf31bd1
karxg <karim4420@aol.com>
2024-08-01 18:49:01 +0300

    fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-"""
-Lockboxes Module
-This module provides a function to determine if all lockboxes can be unlocked.
-"""
+"""Lockboxes"""
 
 
 def canUnlockAll(boxes):
-    """
-    Determine if all lockboxes can be unlocked.
-    """
-    keys = []
-    for box_index in range(1, len(boxes)):
-        collected_keys = [keys.extend(box) for box in boxes[:box_index] + boxes[box_index + 1:]]
-        if box_index in keys:
-            keys = []
+    """LockBoxes Function"""
+    T = []
+    for i in range(1, len(boxes)):
+        order = [T.extend(x) for x in boxes[:i] + boxes[i + 1:]]
+        if i in T:
+            T = []
         else:
             return False
     return True
