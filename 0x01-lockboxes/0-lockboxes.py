afe2a5dbd1a6728946acfa52aac30a8fac361be3
karxg <karim4420@aol.com>
2024-08-01 18:25:06 +0300

    0fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,16 +4,10 @@ Lockboxes Module
 This module provides a function to determine if all lockboxes can be unlocked.
 """
 
+
 def canUnlockAll(boxes):
     """
     Determine if all lockboxes can be unlocked.
-
-    Args:
-        boxes (list of list of int): A list of lists where each inner list contains the keys
-                                     to other boxes.
-
-    Returns:
-        bool: True if all boxes can be unlocked, False otherwise.
     """
     keys = []
     for box_index in range(1, len(boxes)):
